d8a9d82c3032e37559397c52288878a6717cef60
Yuelin Yan <yuelinyan@gmail.com>
2017-04-20 12:57:26 -0600

    start holding your objects

--- src/innerclasses/DotThis.java
+++ src/innerclasses/DotThis.java
@@ -1,21 +1,37 @@
 package innerclasses;
 //: innerclasses/DotThis.java
+
 // Qualifying access to the outer-class object.
 
 public class DotThis {
-  void f() { System.out.println("DotThis.f()"); }
-  public class Inner {
-    public DotThis outer() {
-      return DotThis.this;
-      // A plain "this" would be Inner's "this"
-    }
-  }
-  public Inner inner() { return new Inner(); }
-  public static void main(String[] args) {
-    DotThis dt = new DotThis();
-    DotThis.Inner dti = dt.inner();
-    dti.outer().f();
-  }
-} /* Output:
-DotThis.f()
-*///:~
+	void f() {
+		System.out.println("DotThis.f()");
+	}
+
+	public class Inner {
+		public DotThis outer() {
+			return DotThis.this;
+			// A plain "this" would be Inner's "this"
+		}
+		
+		void f() {
+			System.out.println("Inner.f()");
+		}
+	}
+
+	public Inner inner() {
+		return new Inner();
+	}
+
+	public static void main(String[] args) {
+		DotThis dt = new DotThis();
+		DotThis.Inner dti = dt.inner();
+		dti.outer().f();
+		
+		DotThis.Inner dtii = dt.new Inner();
+		dtii.f();
+		dtii.outer().f();
+	}
+} /*
+	 * Output: DotThis.f()
+	 */// :~
